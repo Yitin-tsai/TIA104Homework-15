1f15a31abe1044d6a8d0cd8c2f9429473935d811
Yitin <yitintsai0906@gmail.com>
2024-09-30 16:10:09 +0800

    first commit

--- src/hw2/TestNineNine.java
+++ src/hw2/TestNineNine.java
@@ -5,14 +5,14 @@ public class TestNineNine {
 	public static void main(String[] args) {
 		
 //		使用for+while迴圈完成99乘法表
-//		int i=1;
-//		for (i = 1; i <= 9; i++) {
-//			int j=1;
-//			while(j<10) { 
-//			System.out.print(i + "*" + j + "=" + i * j + "\t");
-//			j++;}
-//		System.out.println();
-//		}
+		int i=1;
+		for (i = 1; i <= 9; i++) {
+			int j=1;
+			while(j<10) { 
+			System.out.print(i + "*" + j + "=" + i * j + "\t");
+			j++;}
+		System.out.println();
+		}
 		
 //		使用for+do while迴圈完成99乘法表
 //		int i=1;
@@ -26,15 +26,15 @@ public class TestNineNine {
 //		}
 		
 //		使用while+do while迴圈完成99乘法表		
-		int i=0;
-		while(i<=9) {
-			int j =1;
-			do {
-			System.out.print(i + "*" + j + "=" + i * j + "\t");
-			j++;
-			}while (j<=9); 
-		i++;
-		System.out.println();	
-		}
+//		int i=0;
+//		while(i<=9) {
+//			int j =1;
+//			do {
+//			System.out.print(i + "*" + j + "=" + i * j + "\t");
+//			j++;
+//			}while (j<=9); 
+//		i++;
+//		System.out.println();	
+//		}
 	}
 }	
